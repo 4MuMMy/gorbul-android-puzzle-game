e2ede4f6a49ca1c42e9ddd22c53eabfb0d462b05
MuMMy <43711943+4MuMMy@users.noreply.github.com>
2024-05-14 07:12:48 +0300

    Add files via upload

---
+++ gorbul/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gör-Bul: görsel kelime bulmaca")]
+[assembly: AssemblyDescription("Görselde ne olduğunu tahmin ederek kutulardaki gizli harfleri bulmaya çalışmaca.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MuMMy")]
+[assembly: AssemblyProduct("Gör-Bul: görsel kelime bulmaca")]
+[assembly: AssemblyCopyright("Copyright © 2021 - MuMMy")]
+[assembly: AssemblyTrademark("MuMMy")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Application(UsesCleartextTraffic = true)]
